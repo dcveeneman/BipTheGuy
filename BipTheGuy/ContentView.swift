77d39353c99f22345f5f31bb9c75f5d17d5ff399
David Veeneman <davidv@veeneman.com>
2023-11-14 16:05:24 -0800

    2023/11/14 - Chapter 3.1: Initial commit; end of chapter.

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,17 +6,49 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                }
+            Spacer()
+            
+            Button {
+                // TODO: Button action here
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+
         }
         .padding()
     }
+    
+    func playSound(soundName: String) {
+            // Requires AVFAudio import, state variable for AVAudioPlayer
+
+            guard let soundFile = NSDataAsset(name: soundName) else {
+                print("􀀳 Could not read sound file named \(soundName)")
+                return
+            }
+            
+            do {
+                audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                audioPlayer.play()
+            } catch {
+                print("􀀳 ERROR: \(error.localizedDescription) creating audio player.")
+            }
+
+        }
+
 }
 
 #Preview {

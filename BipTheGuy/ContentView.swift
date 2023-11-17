04d22276e453af7ab682fcff0145906a3b496fae
David Veeneman <davidv@veeneman.com>
2023-11-16 17:09:23 -0800

    2023/11/16 - Chapter 3.3: Add photo picker; end of chapter commit.

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -7,16 +7,19 @@
 
 import SwiftUI
 import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
     @State private var animateImage = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
 
     var body: some View {
         VStack {
             Spacer()
             
-            Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit()
                 .scaleEffect(animateImage ? 1.0 : 0.9)
@@ -30,12 +33,28 @@ struct ContentView: View {
     
             Spacer()
             
-            Button {
-                // TODO: Button action here
-            } label: {
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic){
                 Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
             }
-
+            .onChange(of: selectedPhoto) {
+                // We need to:
+                // (1) get the data inside the PhotosPickerItem selectedPhoto;
+                // (2) use that data to create a UIImage;
+                // (3) use the UIImage to create an Image; and
+                // (4) assign that image to bipImage
+                Task {
+                    do {
+                        if let data = try await selectedPhoto?.loadTransferable(type: Data.self) { // (1)
+                            if let uiImage = UIImage(data: data) { // (2)
+                                bipImage = Image(uiImage: uiImage) // (3), (4)
+                            }
+                        }
+                    } catch {
+                        print("􀀳 ERROR: loading failed-- \(error.localizedDescription)")
+                    }
+                }
+            }
+            
         }
         .padding()
     }
